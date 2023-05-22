55b031959ca02e4b74426440ed06a6cd98169d03
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-05-21 21:43:25 -0700

    added large wallpaper image and progress on header/navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 function App() {
 
-  const [count, setCount] = useState(0)
+//   const [count, setCount] = useState(0)
 
-  let plus = () => {
-  setCount((prev) => prev+1)
-}
+//   let plus = () => {
+//   setCount((prev) => prev+1)
+// }
 
   return (
     <>
-      <header className='flex justify-around'>
-        <h1>Final Fantasy XI</h1>
-        <nav className='flex items-center'>
-          <a href="" className='p-4'>Project</a>
-          <a href="" className='p-4'>History</a>
-          <div className='relative'>
+      <header className='flex px-24 justify-between items-center'>
+        <div className='flex'>
+          <img src="./images/icon1.jfif" alt="" className='inline-block h-8 self-end' />
+          <h1 className='text-3xl'>Final Fantasy XI</h1>
+        </div>
+        <nav className='flex text-lg'>
+          <a href="" className='p-4 hover:bg-gray-200 transition duration-300'>Project</a>
+          <a href="" className='p-4 hover:bg-gray-200 transition duration-300'>History</a>
+          <div className='nav__dropdown relative hover:bg-gray-200 transition duration-300'>
             <button type='button' className='p-4'>Jobs</button>
-            <ul className='absolute min-w-max'>
+            <ul className='nav__dropdown-content absolute hidden w-max'>
               <li className='hover:bg-cyan-100'>
                 <a href="" className='px-4 w-full hover:text-gray-500'>Warrior</a></li>
               <li className='hover:bg-cyan-100'>
@@ -33,15 +36,13 @@ function App() {
             </ul>
           </div>
         </nav>
-
-
       </header>
 
+      <hr />
 
-      <h1 className='text-gray-400'>Final Fantasy XI</h1>
-
-      <p>{count}</p>
-      <button onClick={plus}>Increase</button>
+      <div className='w-full'>
+        <img src="/images/wallpaperB.jpg" alt="Final Fantasy XI Wallpaper" className='w-full h-auto' />
+      </div>
     </>
   )
 }
