2cfb0ee1d8e15be5cef8b2485fdc376b637914ec
Christian <ChristianArroyo308@yahoo.com>
2023-06-06 03:20:06 -0700

    styling for the input element in the CommentComponent

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -7,7 +7,7 @@ export default function CommentSection({commentArray}:any) {
       <h2 className="text-2xl text-center mb-6">{commentArray.length} Comments</h2>
       <hr />
       <p className='my-6'>Welcome to the comment section! Comments must be between 4-250 characters long. Drop a "Hello World" and thank you for stopping by.</p>
-      <input type="text" placeholder="What do you think?" className="px-2 py-1 w-full bg-slate-200 border-black rounded-md" />
+      <input type="text" placeholder="What do you think?" className="px-2 py-1 w-full bg-slate-200 border-gray-500 border-[1px] rounded-md" />
       {commentArray.map((comment:string) => <p className='mt-6' key={comment}>{comment}</p>)}
     </div>
   </section>
