dc5867500a660dfd20c7078ed74ff28e70ad8d2e
Christian <ChristianArroyo308@yahoo.com>
2023-06-08 23:48:58 -0700

    making progress on table for forum

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -2,12 +2,12 @@
 
 export default function TableComponent() {
   return (
-    <section className="flex justify-center">
-        <table className='m-8 w-4/5 text-center border-collapse'>
+    <section className="mb-80 flex justify-center">
+        <table className='m-8 w-4/5 text-center'>
             <tr>
                 <th className='px-4 bg-blue-300 text-orange-600 border border-orange-400'>Topics</th>
-                <th className='px-4 bg-blue-300 text-orange-600 border border-orange-400'>Author</th>
-                <th className='px-4 bg-blue-300 text-orange-600 border border-orange-400'>Posts</th>
+                <th className='px-4 bg-blue-300 text-orange-600 border border-orange-400 w-32'>Author</th>
+                <th className='px-4 bg-blue-300 text-orange-600 border border-orange-400 w-16'>Posts</th>
             </tr>
             <tr>
                 <td className="px-4 border border-orange-400">First Topic</td>
