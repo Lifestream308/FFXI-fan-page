28d00a5f806a655bee01327727470722bc59e3cf
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-06-14 03:32:57 -0700

    commenting out entire firebase-config. Trying to fix netlify deploy

--- src/firebase-config.tsx
+++ src/firebase-config.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "@firebase/firestore";
 
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
