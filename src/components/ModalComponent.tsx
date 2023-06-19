52d8c1099804bbfa757f0055132c736c82c45d22
Christian <ChristianArroyo308@yahoo.com>
2023-06-19 01:37:38 -0700

    improved modal styling

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -3,10 +3,11 @@
 export default function ModalComponent({modalMessage, setShowModal}:any) {
   return (
     <>
-    <div className='fixed flex justify-center z-10 w-full h-full bg-gray-400 bg-opacity-50'>
+    <div className='fixed flex justify-center z-10 w-full h-full bg-gray-400 bg-opacity-60'>
         <div className='p-4 w-fit h-fit mt-12 text-center rounded-lg text-black bg-white shadow-2xl'>
-            <p className='mb-5 text-3xl'>{modalMessage}</p>
-            <button className='p-2 text-2xl rounded-lg bg-green-700 text-white' onClick={()=>setShowModal(false)}>Close</button>
+            <p className='mb-4 text-3xl'>{modalMessage}</p>
+            <hr />
+            <button className='mt-4 px-2 py-1 text-2xl rounded-lg bg-red-600 text-white' onClick={()=>setShowModal(false)}>Close</button>
         </div>
     </div>
     </>
