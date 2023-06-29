404a754bc81032542aa8a8871f3fc797416cd1af
Christian <ChristianArroyo308@yahoo.com>
2023-06-29 02:19:55 -0700

    adding content to BGSection. Turning it into About page

--- src/components/BGSection.tsx
+++ src/components/BGSection.tsx
@@ -2,14 +2,24 @@
 
 export default function BGSection() {
   return (
-    <section id="about" className='w-full mt-10 relative'>
-        <img src="/images/wallpaperB.jpg" alt="Final Fantasy XI Wallpaper" className='w-full h-auto' />
-        <div className="absolute top-[15%] left-[12%] p-1 rounded-lg bg-slate-50 bg-opacity-40">
-            <p className="text-4xl">Released in 2002</p>
-            <small>One of the longest running MMO's to date...</small>
+    <>
+      <div className="mt-6">
+        <h2 className="text-lg underline">Website</h2>
+        <p className="">This site was created using React, Typescript, Tailwind, and Firebase.</p>
+      </div>
+      <div className="mt-8">
+        <h2 className="text-lg underline">Final Fantasy XI</h2>
+        <p className="">This Final Fantasy was the first MMO in the series and goes back over 20 years! It has been succeeded by Final Fantasy XIV, but FFXI is still my personal favorite. Whenever I hear the music from FFXI I'm immediately filled with nostalgia.</p>
+      </div>
+      <section id="about" className='w-full mt-4 relative'>
+          <img src="/images/wallpaperB.jpg" alt="Final Fantasy XI Wallpaper" className='w-full h-auto' />
+          <div className="absolute top-[15%] left-[12%] p-1 rounded-lg bg-slate-50 bg-opacity-40">
+              <p className="text-4xl">Released in 2002</p>
+              <small>One of the longest running MMO's to date...</small>
 
-            <p className="mt-6">Enter the world of Vanadiel and team up with others if you want to survive</p>
-        </div>
-    </section>
+              <p className="mt-6">Enter the world of Vanadiel and team up with others if you want to survive</p>
+          </div>
+      </section>
+    </>
   )
 }
