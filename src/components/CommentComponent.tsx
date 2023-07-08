567ce717c232733be14caa3dbb4f739ccab39ce9
Christian <ChristianArroyo308@yahoo.com>
2023-07-07 20:42:39 -0700

    updated forum anchor to a Link element

--- src/components/CommentComponent.tsx
+++ src/components/CommentComponent.tsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function CommentComponent({firebaseItemsDB, messageRef, handleCommentSubmit}:any) {
   return (
@@ -6,7 +7,7 @@ export default function CommentComponent({firebaseItemsDB, messageRef, handleCom
     <div className="w-3/4 m-8 mt-14">
       <h2 className="text-2xl text-center mb-6">{firebaseItemsDB.length} Comments</h2>
       <hr />
-      <p className='my-6'>Welcome to the comment section. Comments must be between 4-250 characters long. Read and post anonymous comments here or create an account and have discussions over on the <a href="" className="text-blue-700">Forum</a>!</p>
+      <p className='my-6'>Welcome to the comment section. Comments must be between 4-250 characters long. Read and post anonymous comments here or create an account and have discussions over on the <Link to="/forum" className="text-blue-700">Forum</Link>!</p>
       <input type="text" placeholder="What do you think?" ref={messageRef} className="px-2 py-1 w-full placeholder-gray-500 bg-slate-200 border-gray-500 border-[1px] rounded-md" /> 
       <button type="button" onClick={()=> handleCommentSubmit()} className="mt-6 p-2 text-white bg-green-700 rounded-md">Post Comment</button>
 
