886fbcc7cd57404f7f7ec1d05493c098632aa6bd
Christian <ChristianArroyo308@yahoo.com>
2023-07-14 22:44:38 -0700

    updating text on aboutComponent. Changing margins near submit post button on Comments

--- src/components/CommentComponent.tsx
+++ src/components/CommentComponent.tsx
@@ -24,9 +24,9 @@ export default function CommentComponent({firebaseItemsDB, messageRef, isSortedB
       <div className="w-3/4 m-8 mt-14">
         <h2 className="text-2xl text-center mb-6">{firebaseItemsDB.length} Comments</h2>
         <hr />
-        <p className='my-6'>Welcome to the comment section. Comments must be between 4-250 characters long. Read and post anonymous comments here or create an account and have discussions over on the <Link to="/forum" className="text-blue-700">Forum</Link>!</p>
+        <p className='my-8'>Welcome to the comment section. Comments must be between 4-250 characters long. Read and post anonymous comments here or create an account and have discussions over on the <Link to="/forum" className="text-blue-700">Forum</Link>!</p>
         <input type="text" placeholder="What do you think?" ref={messageRef} className="px-2 py-1 w-full placeholder-gray-500 bg-slate-200 border-gray-500 border-[1px] rounded-md" /> 
-        <button type="button" onClick={()=> handleCommentSubmit()} className="mt-6 p-2 text-white bg-green-700 rounded-md">Post Comment</button>
+        <button type="button" onClick={()=> handleCommentSubmit()} className="mt-8 p-2 text-white bg-green-700 rounded-md">Post Comment</button>
 
         <div className="my-6 mb-3">
           <small className="text-gray-500">Sort by </small>
