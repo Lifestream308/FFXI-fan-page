4dc8deb49160f49906c79db028ed1fe1f5ad5ff1
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-07-16 10:39:11 -0700

    parallax picture for JobComponent looks good

--- src/components/JobComponent.tsx
+++ src/components/JobComponent.tsx
@@ -2,10 +2,11 @@
 
 export default function JobComponent({job, handleJobChange, jobIndex, jobsArray}:any) {
   return (
-    <main className="flex flex-col justify-center pt-12 px-[5%] gap-12">
-      
-      <div className="flex justify-center items-center h-[32rem] w-full m-auto bg-fixed bg-center bg-contain bg-no-repeat custom-img">
-        <h2 className="p-3 text-4xl text-teal-900 rounded-lg bg-white">Explore Vana'diel</h2>
+    <main className="flex flex-col justify-center pt-8 px-[5%] gap-12">
+
+      <div className="flex flex-col gap-12 justify-center items-center h-[30rem] w-full bg-fixed bg-center bg-cover bg-no-repeat custom-img sm:bg-contain">
+        <h1 className="hidden text-5xl text-white underline sm:flex">Final Fantasy XI Online</h1>
+        <h2 className="text-4xl text-neutral-300 sm:text-5xl">Discover Vana'diel</h2>
       </div>
 
       <div className="flex flex-col gap-6 text-center">
@@ -29,7 +30,7 @@ export default function JobComponent({job, handleJobChange, jobIndex, jobsArray}
             <img src={"/images/" + job.image} alt="Image of FFXI Job" className="mb-8 w-full h-auto p-2 bg-gray-300 border border-black rounded-md" />
           </div>
         </div>
-        <div className="sm:pl-8 sm:w-1/2">
+        <div className="px-2 sm:pl-8 sm:w-1/2">
           <h3 className="text-2xl mb-2 inline-block mr-2 sm:block md:inline-block">2 Hour Ability:</h3>
           <h3 className="text-2xl mb-4 px-3 py-1 w-fit text-white bg-amber-600 rounded-lg inline-block md:inline-block">{job.twoHourAbility}</h3>
           <p className="mt-4 leading-7">{job.twoHourDescription}</p>
