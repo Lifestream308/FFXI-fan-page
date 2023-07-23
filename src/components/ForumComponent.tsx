5a130775b57f7fe9bc1b92dedc92c11efe5ad107
Christian <ChristianArroyo308@yahoo.com>
2023-07-23 01:35:46 -0700

    added small border-top to table rows

--- src/components/ForumComponent.tsx
+++ src/components/ForumComponent.tsx
@@ -6,7 +6,7 @@ export default function ForumComponent() {
         <div className="min-w-full">
             <table className='min-w-full text-left bg-slate-100'>
                 <tbody>
-                <tr>
+                <tr className="border-t">
                     <th className='px-6 py-4 text-sm bg-blue-100 text-indigo-500'>TOPICS</th>
                     <th className='px-6 py-4 text-sm bg-blue-100 text-indigo-500 w-32 hidden sm:table-cell'>AUTHOR</th>
                     <th className='px-6 py-4 text-sm bg-blue-100 text-indigo-500 w-16'>POSTS</th>
@@ -16,12 +16,12 @@ export default function ForumComponent() {
                     <td className="px-6 py-4 text-gray-700 hidden sm:table-cell">NameHere</td>
                     <td className="px-6 py-4 text-gray-700 text-center">8</td>
                 </tr>
-                <tr>
+                <tr className="border-t">
                     <td className="px-6 py-4 text-gray-700 text-left">Second Topic</td>
                     <td className="px-6 py-4 text-gray-700 hidden sm:table-cell">Anonymous</td>
                     <td className="px-6 py-4 text-gray-700 text-center">25</td>
                 </tr>
-                <tr>
+                <tr className="border-t">
                     <td className="px-6 py-4 text-gray-700 text-left">Third Topic</td>
                     <td className="px-6 py-4 text-gray-700 hidden sm:table-cell">Anon</td>
                     <td className="px-6 py-4 text-gray-700 text-center">5</td>
