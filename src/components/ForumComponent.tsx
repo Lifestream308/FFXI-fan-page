9423b39583588fd5588f1159e470851358de04a7
Christian <ChristianArroyo308@yahoo.com>
2023-07-23 00:50:20 -0700

    improve styling on table and removed nth child css from index.css

--- src/components/ForumComponent.tsx
+++ src/components/ForumComponent.tsx
@@ -4,27 +4,27 @@ export default function ForumComponent() {
   return (
     <section className="mt-8 mb-24 mx-8 overflow-hidden rounded-lg">
         <div className="min-w-full">
-            <table className='min-w-full text-center bg-slate-200'>
+            <table className='min-w-full text-left bg-slate-100'>
                 <tbody>
                 <tr>
-                    <th className='px-4 bg-blue-300 text-orange-500'>Topics</th>
-                    <th className='px-4 bg-blue-300 text-orange-500 w-32 hidden sm:table-cell'>Author</th>
-                    <th className='px-4 bg-blue-300 text-orange-500 w-16'>Posts</th>
+                    <th className='px-6 py-4 text-sm bg-blue-100 text-indigo-500'>TOPICS</th>
+                    <th className='px-6 py-4 text-sm bg-blue-100 text-indigo-500 w-32 hidden sm:table-cell'>AUTHOR</th>
+                    <th className='px-6 py-4 text-sm bg-blue-100 text-indigo-500 w-16'>POSTS</th>
                 </tr>
                 <tr>
-                    <td className="px-4 text-left">First Topic</td>
-                    <td className="px-4 hidden sm:table-cell">NameHere</td>
-                    <td className="px-4">8</td>
+                    <td className="px-6 py-4 text-gray-700 text-left">First Topic</td>
+                    <td className="px-6 py-4 text-gray-700 hidden sm:table-cell">NameHere</td>
+                    <td className="px-6 py-4 text-gray-700 text-center">8</td>
                 </tr>
                 <tr>
-                    <td className="px-4 text-left">Second Topic</td>
-                    <td className="px-4 hidden sm:table-cell">Anonymous</td>
-                    <td className="px-4">25</td>
+                    <td className="px-6 py-4 text-gray-700 text-left">Second Topic</td>
+                    <td className="px-6 py-4 text-gray-700 hidden sm:table-cell">Anonymous</td>
+                    <td className="px-6 py-4 text-gray-700 text-center">25</td>
                 </tr>
                 <tr>
-                    <td className="px-4 text-left">Third Topic</td>
-                    <td className="px-4 hidden sm:table-cell">Anon</td>
-                    <td className="px-4">5</td>
+                    <td className="px-6 py-4 text-gray-700 text-left">Third Topic</td>
+                    <td className="px-6 py-4 text-gray-700 hidden sm:table-cell">Anon</td>
+                    <td className="px-6 py-4 text-gray-700 text-center">5</td>
                 </tr>
                 </tbody>
             </table>
