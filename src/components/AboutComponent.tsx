65d3bac2aa752be33fc2d13b9e4ee0be9a80767f
Christian <ChristianArroyo308@yahoo.com>
2023-08-10 03:12:32 -0700

    updated text on about page to include Redux Toolkit

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -6,7 +6,7 @@ export default function AboutComponent() {
       <div className="mt-6 px-6 sm:px-[5%] max-w-5xl">
         <div className="">
           <h2 className="mb-3 text-4xl font-bold">About Website</h2>
-          <p className="leading-8 text-gray-700">This site was created using React, Typescript, Tailwind, and Firebase. This project also makes use of environment variables, react-router-dom, and is hosted on Netlify. After more than 100 commits you can see the code for yourself on <a href="https://github.com/Lifestream308/FFXI-fan-page/tree/main" target="_blank" className="text-blue-700 underline">Github</a>. The job ability descriptions come from the official Final Fantasy XI Strategy Guide.</p>
+          <p className="leading-8 text-gray-700">This site was created using React, Typescript, Tailwind, and Firebase. This project also makes use of environment variables, react-router-dom, Redux Toolkit, and is hosted on Netlify. After more than 100 commits you can see the code for yourself on <a href="https://github.com/Lifestream308/FFXI-fan-page/tree/main" target="_blank" className="text-blue-700 underline">Github</a>. The job ability descriptions come from the official Final Fantasy XI Strategy Guide.</p>
           <img src="/images/strategyGuide.jfif" alt="Picture of FFXI Strategy Guide" className="mt-4 h-auto w-40" />
         </div>
         <div className="mt-10">
