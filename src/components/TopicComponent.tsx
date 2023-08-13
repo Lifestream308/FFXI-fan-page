d0f59f6cb5d65f439b999ccd4679ead65c9f3754
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-08-12 21:27:28 -0700

    styling improvements for TopicComponent

--- src/components/TopicComponent.tsx
+++ src/components/TopicComponent.tsx
@@ -9,11 +9,16 @@ export default function TopicComponent({ forumTopics }:any) {
 
   return (
     <>
-      <div className="mt-8 flex flex-col gap-8">
-        <Link to={'/forum'} className="text-blue-800 self-center">Back to Forum</Link>
-        <p>{ id }</p>
-        <h1 className="text-3xl">{topic.title}</h1>
-        <p>{topic.content}</p>
+    <div className="mt-8 mx-auto p-8 max-w-5xl border-2 border-blue-200 rounded-xl">
+      <div className="flex flex-col gap-8">
+        <Link to={'/forum'} className="text-blue-800 self-center underline underline-offset-4">Back to Forum</Link>
+        <div>
+          <h1 className="text-3xl text-neutral-700">{topic.title}</h1>
+          <small className="text-gray-500">{ id }</small>
+        </div>
+        <hr />
+        <p className="text-xl">{topic.content}</p>
+      </div>
       </div>
     </>
   )
