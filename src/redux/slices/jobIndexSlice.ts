7664913ae32073bd3a5c481b5bdd3e13f8b9ad94
Christian <ChristianArroyo308@yahoo.com>
2023-08-13 16:05:53 -0700

    fixed reducers in jobIndexSlice and imported jobsArray for .length

--- src/redux/slices/jobIndexSlice.ts
+++ src/redux/slices/jobIndexSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
+import jobsArray from '../../util/jobsArray'
 
 export interface jobIndexState {
   value: number
@@ -14,10 +15,10 @@ export const jobIndexSlice = createSlice({
   initialState,
   reducers: {
     prev: (state) => {
-      state.value += 1
+      state.value > 0 ? state.value-1 : jobsArray.length-1
     },
     next: (state) => {
-      state.value -= 1
+      state.value < jobsArray.length-1 ? state.value+1 : 0
     },
     setJobIndex: (state, action: PayloadAction<number>) => {
       state.value = action.payload
