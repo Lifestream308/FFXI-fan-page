d8d63efe55039f55950545dbfa7f8a79d91104ef
Christian <ChristianArroyo308@yahoo.com>
2023-08-23 03:39:17 -0700

    small css styling changes

--- src/components/SignInPageComponent.tsx
+++ src/components/SignInPageComponent.tsx
@@ -5,9 +5,13 @@ export default function SignInPageComponent({user, credentials, register, login,
   return (
       <div className="mt-8 flex flex-col items-center">
         <Link to={'/forum'} className="text-blue-800 self-center underline underline-offset-4">Back to Forum</Link>
-        <p className="mt-4 text-xl">Email: {user?.email}</p>
-        { user && <p className="mt-4 text-xl">UID: {user.uid}</p> }
-        <div className="mt-4 flex flex-col gap-4">
+        { user && 
+        <div>
+            <p className="mt-4 text-xl">Email: {user?.email}</p>
+            <p className="mt-4 text-xl">UID: {user.uid}</p>
+        </div>
+        }
+        <div className="mt-8 flex flex-col gap-4">
             <input type="text" ref={credentials.emailRef} placeholder="Email" className="px-2 py-1 max-w-md placeholder-gray-500 bg-slate-200 border-gray-500 border-[1px] rounded-md" />
             <input type="password" ref={credentials.passwordRef} placeholder="Password" className="px-2 py-1 max-w-md placeholder-gray-500 bg-slate-200 border-gray-500 border-[1px] rounded-md" />
         </div>
