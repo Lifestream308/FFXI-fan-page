74092e26b502de71df52c40cff1b6b0351f05314
Christian <ChristianArroyo308@yahoo.com>
2023-08-23 02:12:32 -0700

    displaying email and uid of user and now only show logout button if user signed in

--- src/components/SignInPageComponent.tsx
+++ src/components/SignInPageComponent.tsx
@@ -5,15 +5,21 @@ export default function SignInPageComponent({user, credentials, register, login,
   return (
       <div className="mt-8 flex flex-col items-center">
         <Link to={'/forum'} className="text-blue-800 self-center underline underline-offset-4">Back to Forum</Link>
-        <p className="mt-4 text-xl">Hello {user?.email}</p>
+        <p className="mt-4 text-xl">Email: {user?.email}</p>
+        { user && <p className="mt-4 text-xl">UID: {user.uid}</p> }
         <div className="mt-4 flex flex-col gap-4">
             <input type="text" ref={credentials.emailRef} placeholder="Email" className="px-2 py-1 max-w-md placeholder-gray-500 bg-slate-200 border-gray-500 border-[1px] rounded-md" />
             <input type="password" ref={credentials.passwordRef} placeholder="Password" className="px-2 py-1 max-w-md placeholder-gray-500 bg-slate-200 border-gray-500 border-[1px] rounded-md" />
         </div>
         <div className="mt-8 flex gap-4">
-            <button onClick={register} className="px-4 py-2 w-fit text-white bg-blue-600 rounded-md" >Register</button>
-            <button onClick={login} className="px-4 py-2 w-fit text-white bg-green-700 rounded-md" >Login</button>
-            <button onClick={logout} className="px-4 py-2 w-fit text-white bg-orange-600 rounded-md" >Logout</button>
+            { !user && 
+            <>
+                <button onClick={register} className="px-4 py-2 w-fit text-white bg-blue-600 rounded-md" >Register</button>
+                <button onClick={login} className="px-4 py-2 w-fit text-white bg-green-700 rounded-md" >Login</button>
+            </> }
+            { user && 
+                <button onClick={logout} className="px-4 py-2 w-fit text-white bg-orange-600 rounded-md" >Logout</button>
+            }
         </div>
     </div>
   )
