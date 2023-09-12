0e31a607fbfb061ecf54e7df9d01bb93c7b191ec
Christian Arroyo <ChristianArroyo308@yahoo.com>
2023-09-12 12:39:16 -0700

    making progress towards an animated hamburger menu button

---
+++ src/components/HamburgerComponent.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+export default function HamburgerComponent() {
+  return (
+    <div className="hamburgerContainer">
+        <label>
+
+            <input type="checkbox" name="" />
+
+            <div className="bar">
+                <span className="top"></span>
+                <span className="middle"></span>
+                <span className="bottom"></span>
+            </div>
+
+        </label>
+    </div>
+  )
+}
